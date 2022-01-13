25a29bf904fb5d14ee6c5cf2a1261e16af7cad74
Asuka <bingkan@outlook.com>
2022-01-14 01:40:31 +0800

    Add Executor base code.

---
+++ TinyRCD/windows/executor.cpp
@@ -0,0 +1,71 @@
+#include "executor.h"
+
+#include <Windows.h>
+
+Executor::Executor()
+{
+}
+
+Executor::~Executor()
+{
+}
+
+bool Executor::Run(void* code)
+{
+	bool ret = false;
+	do
+	{
+		
+		ret  = true;
+	}while(false);
+	return ret;
+}
+
+bool Executor::AddMoudle(const ModuleInfo& mod)
+{
+	modules.emplace_back(mod);
+
+	return SetModuleCodeNX(mod);
+}
+
+bool Executor::SetModuleCodeNX(const ModuleInfo& mod)
+{
+	bool ret = false;
+	do
+	{
+		if (mod.code_ranges.empty())
+		{
+			break;
+		}
+
+		bool has_error = false;
+		for (const auto& range : mod.code_ranges)
+		{
+			if (!range.address || !range.size)
+			{
+				has_error = true;
+				break;
+			}
+
+			// TODO:
+			// To support multiple platform, this protect function should be abstracted.
+
+			// Maybe PAGE_NOACCESS is also OK.
+			DWORD old_protect = 0;
+			BOOL bRet = VirtualProtect(range.address, range.size, PAGE_READONLY, &old_protect);
+			if (!bRet)
+			{
+				has_error = true;
+				break;
+			}
+		}
+
+		if (has_error)
+		{
+			break;
+		}
+
+		ret  = true;
+	}while(false);
+	return ret;
+}
--- TinyRCD/windows/executor.h
+++ TinyRCD/windows/executor.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include <vector>
+#include <string>
+
+struct CodeRange
+{
+	void* address;
+	size_t size;
+};
+
+
+struct ModuleInfo
+{
+	std::string module_name;
+	std::vector<CodeRange> code_ranges;
+};
+
+
+class Executor
+{
+public:
+	Executor();
+	virtual ~Executor();
+
+	bool Run(void* code);
+
+	bool AddMoudle(const ModuleInfo& mod);
+
+private:
+	bool SetModuleCodeNX(const ModuleInfo& mod);
+
+private:
+	std::vector<ModuleInfo> modules;
+};
+
