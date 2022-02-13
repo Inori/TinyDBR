e590817e0b64a687844295f3e22395ef63b840e3
Asuka <bingkan@outlook.com>
2022-02-14 01:10:32 +0800

    Add some incomplete code.

--- TinyDBR/memory_monitor.cpp
+++ TinyDBR/memory_monitor.cpp
@@ -15,5 +15,50 @@ InstructionResult MemoryMonitor::InstrumentInstruction(
 	size_t       bb_address,
 	size_t       instruction_address)
 {
-	return INST_NOTHANDLED;
+	InstructionResult action = INST_NOTHANDLED;
+	do 
+	{
+		
+	} while (false);
+	return action;
+}
+
+bool MemoryMonitor::NeedToHandle(const xed_decoded_inst_t* inst)
+{
+	bool ret = false;
+	do
+	{
+		xed_uint_t mem_op_count = xed_decoded_inst_number_of_memory_operands(inst);
+		if (mem_op_count == 0)
+		{
+			break;
+		}
+
+		xed_category_enum_t category = xed_decoded_inst_get_category(inst);
+		if (m_flags & MonitorFlag::IgnoreCode)
+		{
+			if (category == XED_CATEGORY_CALL || category == XED_CATEGORY_UNCOND_BR)
+			{
+				break;
+			}
+		}
+
+		uint32_t  operand_count = xed_decoded_inst_noperands(inst);
+		const xed_inst_t* xi    = xed_decoded_inst_inst(inst);
+		for (uint32_t i = 0; i != operand_count; ++i)
+		{
+			const xed_operand_t* op = xed_inst_operand(xi, i);
+			xed_operand_enum_t   op_name = xed_operand_name(op);
+
+			if (xed_operand_is_register(op_name))
+			{
+				continue;
+			}
+
+			auto base_reg = xed_decoded_inst_get_base_reg(inst, i)
+		}
+
+		ret  = true;
+	}while(false);
+	return ret;
 }
--- TinyDBR/memory_monitor.h
+++ TinyDBR/memory_monitor.h
@@ -2,7 +2,7 @@
 
 #include "tinydbr.h"
 
-enum class MonitorFlag : uint64_t
+enum MonitorFlag : uint64_t
 {
 	IgnoreCode        = 1 << 0,  // e.g. call [0x1234], jmp [0x1234]
 	IgnoreStack       = 1 << 1,  // e.g. mov rax, [rsp + 0x8]
@@ -23,6 +23,9 @@ protected:
 		Instruction& inst,
 		size_t       bb_address,
 		size_t       instruction_address) override;
+
+private:
+	bool NeedToHandle(const xed_decoded_inst_t* inst);
 
 private:
 	MonitorFlags m_flags = 0;
