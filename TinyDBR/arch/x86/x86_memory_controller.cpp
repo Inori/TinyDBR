de56c3d416b556509fe74b69890afbf40cd628e7
Asuka <bingkan@outlook.com>
2022-02-20 21:14:52 +0800

    Delete old code.

--- TinyDBR/arch/x86/x86_memory_controller.cpp
+++ TinyDBR/arch/x86/x86_memory_controller.cpp
@@ -89,51 +89,6 @@ bool MemoryController::NeedToHandle(Instruction& inst)
 			break;
 		}
 
-/*
-		uint32_t          operand_count = xed_decoded_inst_noperands(xedd);
-		const xed_inst_t* xi            = xed_decoded_inst_inst(xedd);
-		uint32_t          mem_idx       = 0;
-
-		for (uint32_t i = 0; i != operand_count; ++i)
-		{
-			const xed_operand_t* op      = xed_inst_operand(xi, i);
-			xed_operand_enum_t   op_name = xed_operand_name(op);
-
-			if (op_name != XED_OPERAND_MEM0 && op_name != XED_OPERAND_MEM1)
-			{
-				continue;
-			}
-
-			xed_operand_visibility_enum_t visibility = xed_operand_operand_visibility(op);
-			if (visibility != XED_OPVIS_EXPLICIT)
-			{
-				++mem_idx;
-				continue;
-			}
-
-			auto base_reg = xed_decoded_inst_get_base_reg(xedd, mem_idx);
-			++mem_idx;
-			if (m_flags & MonitorFlag::IgnoreStack)
-			{
-				if (base_reg == XED_REG_RSP || base_reg == XED_REG_ESP || base_reg == XED_REG_SP)
-				{
-					need_handle = false;
-					break;
-				}
-			}
-
-			if (m_flags & MonitorFlag::IgnoreRipRelative)
-			{
-				if (base_reg == XED_REG_RIP || base_reg == XED_REG_EIP || base_reg == XED_REG_IP)
-				{
-					need_handle = false;
-					break;
-				}
-			}
-
-		}
-*/
-
 		need_handle  = true;
 	}while(false);
 	return need_handle;
