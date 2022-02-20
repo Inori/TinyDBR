28a98c6ef88ab40d2bc16887fa21ff7e71d0a508
Asuka <bingkan@outlook.com>
2022-02-20 20:21:29 +0800

    Add some test code for instruction rewrite.

--- TinyDBR/arch/x86/x86_memory_controller.cpp
+++ TinyDBR/arch/x86/x86_memory_controller.cpp
@@ -1,5 +1,6 @@
 #include "memory_controller.h"
 #include "x86_helpers.h"
+#include <zasm/zasm.hpp>
 
 MemoryController::MemoryController(MonitorFlags flags):
 	m_flags(flags)
@@ -35,21 +36,69 @@ InstructionResult MemoryController::InstrumentInstruction(
 			break;
 		}
 
-		uint8_t pushfq[] = { 0x9C };
-		uint8_t popfq[]  = { 0x9D };
+		using namespace zasm;
+		using namespace zasm::operands;
 
-		uint8_t encoded_instruction[32] = { 0 };
-		size_t  encoded_length          = Pushaq(
-			ZYDIS_MACHINE_MODE_LONG_64, encoded_instruction, sizeof(encoded_instruction));
+		Program   program(ZydisMachineMode::ZYDIS_MACHINE_MODE_LONG_64);
+		zasm::Assembler a(program);
 
-		WriteCode(module, encoded_instruction, encoded_length);
-		WriteCode(module, pushfq, sizeof(pushfq));
+		a.push(rax);
+		a.push(rcx);
+		a.push(rdx);
+		a.push(rbx);
+		a.push(rbp);
+		a.push(rsi);
+		a.push(rdi);
+		a.push(r8);
+		a.push(r9);
+		a.push(r10);
+		a.push(r11);
+		a.push(r12);
+		a.push(r13);
+		a.push(r14);
+		a.push(r15);
+		a.pushfq();
 
-		WriteCode(module, popfq, sizeof(popfq));
-		encoded_length = Popaq(
-			ZYDIS_MACHINE_MODE_LONG_64, encoded_instruction, sizeof(encoded_instruction));
-		WriteCode(module, encoded_instruction, encoded_length);
-		WriteCode(module, reinterpret_cast<void*>(inst.address), inst.length);
+		a.popfq();
+		a.pop(r15);
+		a.pop(r14);
+		a.pop(r13);
+		a.pop(r12);
+		a.pop(r11);
+		a.pop(r10);
+		a.pop(r9);
+		a.pop(r8);
+		a.pop(rdi);
+		a.pop(rsi);
+		a.pop(rbp);
+		a.pop(rbx);
+		a.pop(rdx);
+		a.pop(rcx);
+		a.pop(rax);
+
+		a.embed(reinterpret_cast<void*>(inst.address), inst.length);
+
+		// Encodes all the nodes.
+		void* code_address = module->instrumented_code_local + module->instrumented_code_allocated;
+		program.serialize(reinterpret_cast<int64_t>(code_address));
+
+		WriteCode(module, (void*)program.getCode(), program.getCodeSize());
+
+		//uint8_t pushfq[] = { 0x9C };
+		//uint8_t popfq[]  = { 0x9D };
+
+		//uint8_t encoded_instruction[32] = { 0 };
+		//size_t  encoded_length          = Pushaq(
+		//          ZYDIS_MACHINE_MODE_LONG_64, encoded_instruction, sizeof(encoded_instruction));
+
+		//WriteCode(module, encoded_instruction, encoded_length);
+		//WriteCode(module, pushfq, sizeof(pushfq));
+
+		//WriteCode(module, popfq, sizeof(popfq));
+		//encoded_length = Popaq(
+		//	ZYDIS_MACHINE_MODE_LONG_64, encoded_instruction, sizeof(encoded_instruction));
+		//WriteCode(module, encoded_instruction, encoded_length);
+		//WriteCode(module, reinterpret_cast<void*>(inst.address), inst.length);
 
 		action = INST_HANDLED;
 	} while (false);
